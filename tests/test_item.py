d8a3c917fd50cb0a196f57a74d155bb977e4e225
dmitry-sharushkin <dimasharushkin@gmail.com>
2023-06-03 17:59:51 +0300

    add tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,16 +1,7 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
 from src.item import Item
-
-
-def test_instantiate_from_csv():
-    Item.instantiate_from_csv()
-    assert len(Item.all) == 5
-
-
-def test_string_to_number():
-    assert Item.string_to_number('5') == 5
-    assert Item.string_to_number('5.0') == 5
-    assert Item.string_to_number('5.5') == 5
 
 
 class TesteItem:
@@ -30,3 +21,39 @@ class TesteItem:
         item2 = Item("Ноутбук", 20000, 5)
         assert item1 in Item.all
         assert item2 in Item.all
+
+
+def test_name():
+    item = Item('Телефон', 10000, 5)
+
+    item.name = 'Смартфон'
+    assert item.name == 'Смартфон'
+    item.name = 'Смартфон2'
+    assert item.name == 'Смартфон2'
+    with pytest.raises(Exception):
+        item.name = 'Длина наименования товара превышает 10 символов'
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
+
+
+def test_repr():
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Компьютер", 20000, 5)
+    assert repr(item1) == "Item('Смартфон', 10000, 20)"
+    assert repr(item2) == "Item('Компьютер', 20000, 5)"
+
+
+def test_str():
+    item1 = Item("Смартфон", 10000, 20)
+    item2 = Item("Компьютер", 20000, 5)
+    assert str(item1) == 'Смартфон'
+    assert str(item2) == 'Компьютер'
