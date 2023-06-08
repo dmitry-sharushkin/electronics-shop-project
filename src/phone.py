38f72b61513c5b69404c9b8a96ac6fbffd889003
dmitry-sharushkin <dimasharushkin@gmail.com>
2023-06-08 22:34:26 +0300

    adding docstrings

--- src/phone.py
+++ src/phone.py
@@ -3,10 +3,21 @@ from src.item import Item
 
 class Phone(Item):
     def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
+        """
+        Создание экземпляра класса phone.
+
+        :param name: Название товара.
+        :param price: Цена за единицу товара.
+        :param quantity: Количество товара в магазине.
+        :param number_of_sim: Rоличество поддерживаемых сим-карт
+        """
         super().__init__(name, price, quantity)
         self.__number_of_sim = number_of_sim
 
     def __repr__(self):
+        """
+        Отображения информации об объекте класса в режиме отладки
+        """
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
 
     @property
