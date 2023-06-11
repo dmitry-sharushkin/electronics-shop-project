75a049177ab545a4500d9b98820bf0791934949a
dmitry-sharushkin <dimasharushkin@gmail.com>
2023-06-11 11:59:28 +0300

    adding tests

---
+++ tests/test_keyboard.py
@@ -0,0 +1,17 @@
+from src.keyboard import Keyboard, MixinLanguage
+from src.item import Item
+
+
+def test_init():
+    kb = Keyboard('Dark Project KD87A', 9600, 5)
+    assert str(kb) == "Dark Project KD87A"
+    assert str(kb.language) == "EN"
+
+
+def test_change_lang():
+    kb = Keyboard('Dark Project KD87A', 9600, 5)
+    kb.change_lang()
+    assert str(kb.language) == "RU"
+
+    kb.change_lang().change_lang()
+    assert str(kb.language) == "RU"
