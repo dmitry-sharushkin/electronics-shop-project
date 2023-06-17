78ca421dd6df8d8cfd33a52cfed28159a7b8ce74
dmitry-sharushkin <dimasharushkin@gmail.com>
2023-06-17 09:56:57 +0300

    adding new functionality

--- src/keyboard.py
+++ src/keyboard.py
@@ -2,25 +2,20 @@ from src.item import Item
 
 
 class MixinLanguage:
-    LANGUAGES = {
-        'EN': 'RU',
-        'RU': 'EN'
-    }
-
-    def __init__(self):
-        self._language = 'EN'
+    __language = 'EN'
 
     @property
     def language(self):
-        return self._language
+        return self.__language
 
     def change_lang(self):
-        if self._language in self.LANGUAGES:
-            self._language = self.LANGUAGES[self._language]
+        if self.__language == 'EN':
+            self.__language = 'RU'
+        else:
+            self.__language = 'EN'
         return self
 
 
 class Keyboard(Item, MixinLanguage):
     def __init__(self, name: str, price: float, quantity: int):
         super().__init__(name, price, quantity)
-        self._language = 'EN'
